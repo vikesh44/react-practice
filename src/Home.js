0b9140c6a0cf5103c3943c3cd616f960a8a144df
VikeshkumarMewada <VikeshkumarVMewada@Eaton.com>
2024-07-21 15:55:11 +0530

    .

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,25 @@
+import { useState } from "react";
+
 const Home = () => {
-    return ( 
-        <div className="home">
-            <h2>I am from Home page</h2>
-        </div>
-     );
-}
- 
+  const [name, setName] = useState("Niya");
+
+  const handleClick = (e) => {
+    setName("Poonam");
+    console.log("Hello, " + name);
+  };
+
+  const handleClick2 = (name) => {
+    console.log("Hello, " + name);
+  };
+
+  return (
+    <div className="home">
+      <h2>I am from Home page</h2>
+      <button onClick={handleClick}>Click Me</button>
+      <p>{name}</p>
+      <button onClick={() => handleClick2("Vikesh")}>Click Me 2</button>
+    </div>
+  );
+};
+
 export default Home;
